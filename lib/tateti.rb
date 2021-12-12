facd96c5efde408ce00a7eef7ea0367bee49e8e6
Tomas Miguez <tomasmiguez99@gmail.com>
2021-12-12 14:46:43 -0300

    ended? implementado

--- lib/tateti.rb
+++ lib/tateti.rb
@@ -9,15 +9,32 @@ class Tateti
 
   def play(player, position)
     position_x, position_y = position
+
     raise TatetiError.new("Invalid square <#{ position.join(", ") }>")  unless position_x.between?(0, 2) and position_y.between?(0, 2)
-
-    raise TatetiError.new("It's not player <#{ player }> turn to play") if player != @next_player
-
     raise TatetiError.new("The square <#{ position.join(", ") }> is already occupied") unless @board[position_x][position_y] == :empty_square
+    raise TatetiError.new("It's not player <#{ player }> turn to play") if player != @next_player
 
     board[position_x][position_y] = player
 
     @next_player = (@next_player + 1) % 2
+  end
+
+  def ended?
+    if @board.any?([0, 0, 0]) or @board.any?([1, 1, 1]) then return true end
+    
+    for i in (0..2)
+      column = [@board[0][i], @board[1][i], @board[2][i]]
+
+      if column == [0, 0, 0] or column == [1, 1, 1] then return true end
+    end
+
+    left_righ_diagonal = [@board[0][0], @board[1][1], @board[2][2]]
+    if left_righ_diagonal == [0, 0, 0] or left_righ_diagonal == [1, 1, 1] then return true end
+    
+    right_left_diagonal = [@board[0][2], @board[1][1], @board[2][0]]
+    if right_left_diagonal == [0, 0, 0] or right_left_diagonal == [1, 1, 1] then return true end
+
+    return false
   end
 
 end
--- spec/tateti_spec.rb
+++ spec/tateti_spec.rb
@@ -4,65 +4,115 @@ def emptyBoard
   Array.new(3) { Array.new(3, :empty_square)}
 end
 
-RSpec.describe Tateti, "#board" do
+RSpec.describe Tateti, "#play" do
+  before(:each) do
+    @tateti = Tateti.new
+  end
+
   context "just created" do
     it "board is empty" do
-      tateti = Tateti.new
-
-      expect(tateti.board).to eq emptyBoard
+      expect(@tateti.board).to eq emptyBoard
     end
 
     it "first player can play valid square" do
-      tateti = Tateti.new
-
-      tateti.play(0, [0, 0])
+      @tateti.play(0, [0, 0])
 
       expectedBoard = emptyBoard
       expectedBoard[0][0] = 0
-      expect(tateti.board).to eq expectedBoard
+      expect(@tateti.board).to eq expectedBoard
     end
 
     it "first player can't play invalid square" do
-      tateti = Tateti.new
-
       expect {
-        tateti.play(0, [5, 0])
+        @tateti.play(0, [5, 0])
       }.to raise_error(TatetiError)
     end
 
     it "second player can't play first" do
-      tateti = Tateti.new
-
       expect {
-        tateti.play(1, [0, 0])
+        @tateti.play(1, [0, 0])
       }.to raise_error(TatetiError)
     end
   end
 
   context "first turn played" do
     it "second player can play" do
-      tateti = Tateti.new
-      tateti.play(0, [0, 0])
+      @tateti.play(0, [0, 0])
 
-      tateti.play(1, [1, 0])
+      @tateti.play(1, [1, 0])
 
       expectedBoard = emptyBoard
       expectedBoard[0][0] = 0
       expectedBoard[1][0] = 1
-      expect(tateti.board).to eq expectedBoard
+      expect(@tateti.board).to eq expectedBoard
     end
 
     it "players can't overwrite occupied squares" do
-      tateti = Tateti.new
-      tateti.play(0, [0, 0])
+      @tateti.play(0, [0, 0])
 
       expect {
-        tateti.play(1, [0, 0])
+        @tateti.play(1, [0, 0])
       }.to raise_error(TatetiError)
     end
   end
 end
 
-RSpec.describe Tateti, "#winning" do
-  context ""
+RSpec.describe Tateti, "#ending" do
+  before(:each) do
+    @tateti = Tateti.new
+  end
+
+  context "just created" do
+    it "game hasn't ended" do
+      expect(@tateti.ended?).to eq false
+    end
+  end
+
+  context "a player won with complete row" do
+    it "game ended" do
+      @tateti.play(0, [0, 0])
+      @tateti.play(1, [1, 0])
+      @tateti.play(0, [0, 1])
+      @tateti.play(1, [1, 1])
+      @tateti.play(0, [0, 2])
+
+      expect(@tateti.ended?).to eq true
+    end
+  end
+
+  context "a player won with complete column" do
+    it "game ended" do
+      @tateti.play(0, [0, 0])
+      @tateti.play(1, [0, 2])
+      @tateti.play(0, [1, 0])
+      @tateti.play(1, [1, 2])
+      @tateti.play(0, [2, 0])
+
+      expect(@tateti.ended?).to eq true
+    end
+  end
+
+  context "a player won with complete left to right diagonal" do
+    it "game ended" do
+      @tateti.play(0, [0, 0])
+      @tateti.play(1, [0, 2])
+      @tateti.play(0, [1, 1])
+      @tateti.play(1, [1, 2])
+      @tateti.play(0, [2, 2])
+
+      expect(@tateti.ended?).to eq true
+    end
+  end
+
+  context "a player won with complete right to left diagonal" do
+    it "game ended" do
+      @tateti.play(0, [0, 2])
+      @tateti.play(1, [0, 1])
+      @tateti.play(0, [1, 1])
+      @tateti.play(1, [1, 2])
+      @tateti.play(0, [2, 0])
+
+      expect(@tateti.ended?).to eq true
+    end
+  end
 end
