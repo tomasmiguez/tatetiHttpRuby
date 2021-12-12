63025939042c454e0ccdbf5abd091602c0f8004f
Tomas Miguez <tomasmiguez99@gmail.com>
2021-12-12 15:21:54 -0300

    Modificado para que checkee si alguien gano en cada turno.

--- lib/tateti.rb
+++ lib/tateti.rb
@@ -1,10 +1,42 @@
 class Tateti
+  Left_right_diagonal_positions = [[0,0], [1,1], [2,2]]
+  Right_left_diagonal_positions = [[0,2], [1,1], [2,0]]
+
   attr_reader :board
   attr_reader :next_player
+  attr_reader :winner
+  attr_reader :turn
 
   def initialize
     @board = Array.new(3) { Array.new(3, :empty_square)}
     @next_player = 0
+    @turn = 0
+    @winner = nil
+  end
+
+  def was_winning_move?(position)
+    position_x, position_y = position
+    player = @board[position_x][position_y]
+    
+    #Si todas las posiciones de la fila son iguales, gano.
+    if @board[position_x].uniq.size <= 1 then 
+      return player 
+    end
+    
+    column = [@board[0][position_y], @board[1][position_y], @board[2][position_y]]
+    if column.uniq.size <= 1 then return player end
+
+    if Left_right_diagonal_positions.include?(position) 
+      left_righ_diagonal = Array.new(3) { |i| @board[i][i] }
+      if left_righ_diagonal.uniq.size <= 1 then return player end
+    end
+
+    if Right_left_diagonal_positions.include?(position) 
+      right_left_diagonal = [@board[0][2], @board[1][1], @board[2][0]]
+      if right_left_diagonal.uniq.size <= 1 then return player end
+    end
+
+    return nil
   end
 
   def play(player, position)
@@ -16,28 +48,20 @@ class Tateti
 
     board[position_x][position_y] = player
 
+    if @turn >= 4 #Primer turno en el que alguien puede ganar
+      @winner = was_winning_move?(position)
+    end
+
     @next_player = (@next_player + 1) % 2
+    @turn += 1
   end
 
   def ended?
-    if @board.any?([0, 0, 0]) or @board.any?([1, 1, 1]) then return true end
-    
-    for i in (0..2)
-      column = [@board[0][i], @board[1][i], @board[2][i]]
-
-      if column == [0, 0, 0] or column == [1, 1, 1] then return true end
-    end
-
-    left_righ_diagonal = [@board[0][0], @board[1][1], @board[2][2]]
-    if left_righ_diagonal == [0, 0, 0] or left_righ_diagonal == [1, 1, 1] then return true end
-    
-    right_left_diagonal = [@board[0][2], @board[1][1], @board[2][0]]
-    if right_left_diagonal == [0, 0, 0] or right_left_diagonal == [1, 1, 1] then return true end
-
-    return false
+    if @winner then return true else false end
   end
 
 end
+
 
 class TatetiError < StandardError
 end
